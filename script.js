37111cae9ae7585c78bcec97999b3f7813413d2b
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-17 20:36:19 +0100

    add class for listTask, querySelectorAll for lists and button

---
+++ script.js
@@ -0,0 +1,14 @@
+// Buttons
+
+const addButton = document.querySelectorAll(".add_task-button")
+
+
+
+// List
+
+const backlogItem = document.querySelectorAll(".list_item-backlog");
+const progressItem = document.querySelectorAll(".list_item-progress");
+const completeItem = document.querySelectorAll(".list_item-complete");
+const onHoldItem = document.querySelectorAll(".list_item-onhold");
+
+
