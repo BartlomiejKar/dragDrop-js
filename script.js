6c1ae91ec2d96e24544567ab962089d46d2d20b4
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-18 20:30:29 +0100

    add toggleInputContainer and eventListener for button cancel in input

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Buttons
 
-const addButton = document.querySelectorAll(".add_task-button")
+const addButton = document.querySelectorAll(".add_task-button");
+const addInputButton = document.querySelector(".input_button-add");
+const cancelInputButton = document.querySelector(".input_button-cancel")
 
 
 
@@ -11,10 +13,28 @@ const progressItem = document.querySelectorAll(".list_item-progress");
 const completeItem = document.querySelectorAll(".list_item-complete");
 const onHoldItem = document.querySelectorAll(".list_item-onhold");
 
+// Input
+
+
+const inputContainer = document.querySelector(".container_input")
+
+
+toggleInputContainer = () => {
+    if (inputContainer.classList.contains("container_input")) {
+        inputContainer.classList.remove("container_input");
+        inputContainer.classList.add("container_remove-input")
+    } else {
+        inputContainer.classList.remove("container_remove-input")
+        inputContainer.classList.add("container_input")
+    }
+
+}
 
 
 checkClassList = (button) => {
+
     if (button.classList.contains("button_backlog")) {
+        toggleInputContainer()
         console.log("backlog")
     } else if (button.classList.contains("button_progress")) {
         console.log("progress")
@@ -31,9 +51,14 @@ AddTask = (button) => {
     checkClassList(button)
 }
 
+// EventListeners
+
 const buttons = addButton.forEach(button => {
 
     button.addEventListener("click", () => AddTask(button))
 })
+
+cancelInputButton.addEventListener("click", toggleInputContainer)
+
 
 
