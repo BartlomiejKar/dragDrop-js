83ad24510aced3545b4d31c5dc05877016e314ff
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-18 20:08:47 +0100

    add addTask and function CheckList() which check classList after click button

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ const completeItem = document.querySelectorAll(".list_item-complete");
 const onHoldItem = document.querySelectorAll(".list_item-onhold");
 
 
+
+checkClassList = (button) => {
+    if (button.classList.contains("button_backlog")) {
+        console.log("backlog")
+    } else if (button.classList.contains("button_progress")) {
+        console.log("progress")
+    } else if (button.classList.contains("button_onhold")) {
+        console.log("onhold")
+    } else if (button.classList.contains("button_complete")) {
+        console.log("complete")
+    } else {
+        return
+    }
+}
+
+AddTask = (button) => {
+    checkClassList(button)
+}
+
+const buttons = addButton.forEach(button => {
+
+    button.addEventListener("click", () => AddTask(button))
+})
+
+
