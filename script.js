b01dc990d738249876a238cceac3e30b49979fe5
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-18 21:31:25 +0100

    add disabled div input container

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// containers
+const backlogContainer = document.querySelector(".list_container-backlog");
+const progressContainer = document.querySelector(".list_container-progress");
+const completeContainer = document.querySelector(".list_container-complete");
+const onHoldContainer = document.querySelector(".list_container-onhold");
+
 // Buttons
 
 const addButton = document.querySelectorAll(".add_task-button");
 const addInputButton = document.querySelector(".input_button-add");
-const cancelInputButton = document.querySelector(".input_button-cancel")
+const cancelInputButton = document.querySelector(".input_button-cancel");
+
 
 
 
@@ -15,40 +22,61 @@ const onHoldItem = document.querySelectorAll(".list_item-onhold");
 
 // Input
 
-
+const input = document.getElementById("input")
 const inputContainer = document.querySelector(".container_input")
 
+
+addTaskInputButton = (e) => {
+    e.preventDefault();
+    let value = input.value
+    console.log(value);
+    value = "";
+
+}
+
+disabledButtons = () => {
+    addButton.forEach(el => el.disabled ? el.disabled = false : el.disabled = true)
+}
 
 toggleInputContainer = () => {
     if (inputContainer.classList.contains("container_input")) {
         inputContainer.classList.remove("container_input");
         inputContainer.classList.add("container_remove-input")
     } else {
-        inputContainer.classList.remove("container_remove-input")
-        inputContainer.classList.add("container_input")
+        inputContainer.classList.remove("container_remove-input");
+        inputContainer.classList.add("container_input");
+        disabledButtons()
     }
 
 }
 
 
 checkClassList = (button) => {
-
     if (button.classList.contains("button_backlog")) {
-        toggleInputContainer()
-        console.log("backlog")
+        toggleInputContainer();
+        backlogContainer.appendChild(inputContainer);
+
     } else if (button.classList.contains("button_progress")) {
-        console.log("progress")
+        toggleInputContainer();
+        progressContainer.appendChild(inputContainer);
+
     } else if (button.classList.contains("button_onhold")) {
-        console.log("onhold")
+        toggleInputContainer();
+        onHoldContainer.appendChild(inputContainer);
+
     } else if (button.classList.contains("button_complete")) {
-        console.log("complete")
+        toggleInputContainer();
+        completeContainer.appendChild(inputContainer);
+
     } else {
         return
     }
 }
 
 AddTask = (button) => {
-    checkClassList(button)
+    checkClassList(button);
+    disabledButtons()
+
 }
 
 // EventListeners
@@ -58,7 +86,8 @@ const buttons = addButton.forEach(button => {
     button.addEventListener("click", () => AddTask(button))
 })
 
-cancelInputButton.addEventListener("click", toggleInputContainer)
+cancelInputButton.addEventListener("click", toggleInputContainer);
+input.addEventListener("input", addTaskInputButton)
 
-
+// addInputButton.addEventListener("onChange", addTaskInputButton)
 
